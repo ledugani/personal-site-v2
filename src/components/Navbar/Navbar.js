cb1838121b20e8272ee2914056b4483b65805319
Tom Dugan <thomas@aadwebb.com>
2019-10-08 15:34:21 -0500

    navbar updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,21 +2,40 @@ import React from 'react';
 import './Navbar.css';
 
 class Navbar extends React.Component {
-
   render () {
     return (
       <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Experience</a></li>
-              <li><a href="#">Projects</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
-          </div>
-        </div>
+        <ul className="navigation-list">
+          <a href="/">
+            <li className="nav-btn nav-home">
+              <p>Home</p>
+            </li>
+          </a>
+
+          <a href="/">
+            <li className="nav-btn nav-experience">
+              <p>Experience</p>
+            </li>
+          </a>
+
+          <a href="/">
+            <li className="nav-btn nav-projects">
+              <p>Projects</p>
+            </li>
+          </a>
+
+          <a href="/">
+            <li className="nav-btn nav-blog">
+              <p>Blog</p>
+            </li>
+          </a>
+
+          <a href="/">
+            <li className="nav-btn nav-contact">
+              <p>Contact</p>
+            </li>
+          </a>
+        </ul>
       </nav>
     );
   }
