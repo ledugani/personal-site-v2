bd79ec002299158d587a952085643be8d8dd22f5
Tom Dugan <thomas@aadwebb.com>
2019-10-17 12:03:53 -0500

    responsive bio

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,7 @@ class Home extends React.Component {
           </div>
         </div>
         <div className="bio">
-          <p className="bio-text">Hello! My name is Thomas Dugan and I am a Software and Web Developer with almost 6 years of experience in web-based languages. I also have quite a bit of experience utilizing popular Content Management Systems and JavaScript libraries. My strengths include designing applications and troubleshooting complex client integrations and implementations while thoroughly documenting recurring processes. I'm currently seeking to expand my breadth of knowledge and experience in Software-oriented roles with a company offering a team-based, fast-paced environment with opportunities for growth.</p>
+          <p className="bio-text">Hello! My name is Thomas Dugan and I am a Software and Web Developer with almost 4 years of experience in web-based languages. I also have quite a bit of experience utilizing popular Content Management Systems and JavaScript libraries. My strengths include designing applications and troubleshooting complex client integrations and implementations while thoroughly documenting recurring processes. I'm currently seeking to expand my breadth of knowledge and experience in Software-oriented roles with a company offering a team-based, fast-paced environment with opportunities for growth.</p>
         </div>
       </div>
     );
