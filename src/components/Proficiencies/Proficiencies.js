f79af9d69343e4dbf912abfdfcff6bb69500f2d6
Tom Dugan <thomas@aadwebb.com>
2019-10-17 13:54:17 -0500

    home finished, created proficiencies

---
+++ src/components/Proficiencies/Proficiencies.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Proficiencies.css';
+
+class Proficiencies extends React.Component {
+
+  render () {
+    return (
+      <div className="proficiencies">
+        Proficiencies
+      </div>
+    );
+  }
+};
+
+export default Proficiencies;
