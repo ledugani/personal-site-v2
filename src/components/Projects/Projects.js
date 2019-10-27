0561cb330d948194feead1d2b52adec2a4cca381
Tom Dugan <thomas@aadwebb.com>
2019-10-27 17:50:14 -0500

    organized data structure

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Collapsible from 'react-collapsible';
+
 import './Projects.css';
 
 class Projects extends React.Component {
 
   render () {
+    const MyProjects =
     return (
       <div className="projects">
         <h1>Full Stack Projects</h1>
+        <Collapsible>
+          <ul className="responsibilities">
+            {data.responsibilities.map((item, index) => (
+              <li key={index} item={item}>{item}</li>
+            ))}
+          </ul>
+        </Collapsible>
       </div>
     );
   }
