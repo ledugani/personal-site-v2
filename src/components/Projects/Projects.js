0ca38464bd38cd416abffb5b21b05a456fd2c4e6
Tom Dugan <thomas@aadwebb.com>
2019-11-05 10:12:10 -0600

    wip from previous

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,23 +16,30 @@ class Projects extends React.Component {
             <div className="project-container">
 
               <div className="left">
+
                 <img src={placeholder} alt="placeholder" className="placeholder" />
+
                 <div className="buttons-container">
 
-                  <button className="left-button button btn btn-2 btn-2a">Demo</button>
+                  <button className="left-button button btn btn-2 btn-2a" href={data.demoLink}>Demo</button>
 
-                  <button className="right-button button btn btn-2 btn-2a">Code</button>
+                  <button className="right-button button btn btn-2 btn-2a" href={data.githubLink}>Code</button>
 
                 </div>
               </div>
 
               <div className="right">
-                <p className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="project-description">{data.description}</p>
+                <br />
                 <ul className="project-details">
 
-                  <li>list item 1</li>
-                  <li>list item 2</li>
-                  <li>list item 3</li>
+                {
+                  data.techUsed.map((tech) => {
+                    return (
+                      <li className="technologies">{tech}</li>
+                    )
+                  })
+                }
 
                 </ul>
               </div>
